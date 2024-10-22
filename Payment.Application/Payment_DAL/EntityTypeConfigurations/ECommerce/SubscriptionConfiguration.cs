976173f1eb32e255be4398bac51bf3002a7f15ba
ksenia <stelmak.k.d@gmail.com>
2024-10-22 14:44:25 +0300

    init

---
+++ Payment.Application/Payment_DAL/EntityTypeConfigurations/ECommerce/SubscriptionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Payment.Domain.ECommerce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Application.Payment_DAL.EntityTypeConfigurations.ECommerce
+{
+    public class SubscriptionConfiguration : IEntityTypeConfiguration<Subscription>
+    {
+        public void Configure(EntityTypeBuilder<Subscription> builder)
+        {
+            builder.HasOne(sub => sub.Product).WithMany(p => p.Subscription);
+        }
+    }
+}
