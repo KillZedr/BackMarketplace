9632246893a06fcb980c5b742d91d73f07f99bb2
ksenia <stelmak.k.d@gmail.com>
2024-10-22 13:50:29 +0300

    added product & user dtos

---
+++ Payment.Domain/DTOs/ProductCreationDto.cs
@@ -0,0 +1,18 @@
+﻿using Payment.Domain.ECommerce;
+using Payment.Domain.PayProduct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Domain.DTOs
+{
+    public class ProductCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string categoryName { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
---
+++ Payment.Domain/DTOs/UserCreationDto.cs
@@ -0,0 +1,21 @@
+﻿using Payment.Domain.ECommerce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Domain.DTOs
+{
+    internal class UserCreationDto
+    {
+        public required string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Сountry { get; set; }
+        public required string Address { get; set; }
+        public required string PhoneNumber { get; set; }
+
+        public int basketId { get; set; }
+    }
+}
