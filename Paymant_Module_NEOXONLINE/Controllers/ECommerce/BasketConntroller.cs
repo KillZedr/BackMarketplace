cf35d42653ba47dc5f0615d6cc2a8b94c7f66783
IliaTsarikov <https://github.com/IliaTsarikov>
2024-10-24 13:42:21 +0300

    add controllers with methods Get, Delete

---
+++ Paymant_Module_NEOXONLINE/Controllers/ECommerce/BasketConntroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Payment.Application.Payment_DAL.Contracts;
+using Payment.Domain.ECommerce;
+
+namespace Paymant_Module_NEOXONLINE.Controllers.ECommerce
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketConntroller : ControllerBase
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BasketConntroller(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("AllBaskets")]
+
+        public async Task<IActionResult> GetAllBascets ()
+        {
+            var repoBasket = await _unitOfWork.GetAllIncluding<Basket>(b => b.ProductInBasket).ToListAsync();
+            return Ok(repoBasket);
+        }
+    }
+}
---
+++ Paymant_Module_NEOXONLINE/Controllers/ECommerce/ProductInBasketController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Payment.Application.Payment_DAL.Contracts;
+using Payment.Domain.ECommerce;
+
+namespace Paymant_Module_NEOXONLINE.Controllers.ECommerce
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductInBasketController : ControllerBase
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductInBasketController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet("AllProductInBasket")]
+
+        public async Task<IActionResult> GetAllProductInBasket()
+        {
+            var repoProductInBasket = await _unitOfWork.GetAllIncluding<ProductInBasket>(pib => pib.Product).ToListAsync();
+            return Ok(repoProductInBasket);
+        }
+
+
+
+    }
+}
