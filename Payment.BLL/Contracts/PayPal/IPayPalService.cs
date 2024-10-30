d175e525693f124f3dd8a2f59d4bf0a6ae95bfde
IliaTsarikov <https://github.com/IliaTsarikov>
2024-10-31 02:05:50 +0300

    create interface

---
+++ Payment.BLL/Contracts/PayPal/IPayPalService.cs
@@ -0,0 +1,21 @@
+﻿using Payment.Domain.ECommerce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayPalPayment = PayPal.Api.Payment;
+
+
+namespace Payment.BLL.Contracts.PayPal
+{
+    public interface IPayPalService : IService
+    {
+        Task<PayPalPayment> CreatePayment(PaymentBasket basket);
+
+        Task<bool> CancelPayment(string paymentId);
+
+
+        PayPalPayment GetPayment(string paymentId);
+    }
+}
