26d1f9016b647a7123049201d571f894b5aacd56
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2024-11-13 12:10:19 +0300

    Add sepa DTO

---
+++ Payment.BLL/DTOs/SepaPaymentRequest.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Payment.BLL.DTOs
+{
+    public class SepaPaymentRequest
+    {
+        public string Iban { get; set; }
+        public string IpAddress { get; set; }
+        public string UserAgent { get; set; }
+    }
+}
