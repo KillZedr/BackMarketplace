295087cb6b74809f82709ea4b644420a39408982
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2024-11-18 13:01:26 +0300

    Add migrations

---
+++ Payment.Application/Migrations/20241118084827_PymentMetodInStripeDonation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Payment.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class PymentMetodInStripeDonation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "StripeDonation",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "StripeDonation");
+        }
+    }
+}
