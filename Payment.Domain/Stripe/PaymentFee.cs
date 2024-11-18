d8333cf07488807226196700472e6f882ee12b92
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2024-11-18 17:07:14 +0300

    Create PaymentFee Entity for fees

---
+++ Payment.Domain/Stripe/PaymentFee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Domain.Stripe
+{
+    public class PaymentFee : Entity<int>
+    {
+        public int Id { get; set; }
+        public string PaymentMethod { get; set; }
+        public decimal PercentageFee { get; set; }
+        public decimal FixedFee { get; set; }
+        public string Currency { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
