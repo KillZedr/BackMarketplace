3e640db83c0bbc61b1c5f3d709337b1317f7efd3
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2024-11-18 17:09:22 +0300

    Create migrations with PaymentFee

--- Payment.Application/Migrations/20241118121456_PaymentFee.Designer.cs
+++ Payment.Application/Migrations/20241118121456_PaymentFee.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Payment.Application;
@@ -11,9 +12,11 @@ using Payment.Application;
 namespace Payment.Application.Migrations
 {
     [DbContext(typeof(Payment_DbContext))]
-    partial class Payment_DbContextModelSnapshot : ModelSnapshot
+    [Migration("20241118121456_PaymentFee")]
+    partial class PaymentFee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -318,10 +321,6 @@ namespace Payment.Application.Migrations
                     b.HasIndex("PaymentMethod")
                         .IsUnique()
                         .HasDatabaseName("IX_PaymentMethod_Unique");
-
-                    b.HasIndex("PaymentMethod", "Currency")
-                        .IsUnique()
-                        .HasDatabaseName("IX_PaymentMethod_Currency_Unique");
 
                     b.ToTable("PaymentFees", (string)null);
                 });
---
+++ Payment.Application/Migrations/20241118131351_PaymentFeeUpdate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Payment.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentFeeUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentMethod_Currency_Unique",
+                table: "PaymentFees",
+                columns: new[] { "PaymentMethod", "Currency" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PaymentMethod_Currency_Unique",
+                table: "PaymentFees");
+        }
+    }
+}
