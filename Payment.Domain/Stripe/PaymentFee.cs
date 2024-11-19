670933099ce2908613dda5b8f465b508ba2aed7f
DenisBezdolny <d.i.d.k.o.d.a@gmail.com>
2024-11-19 11:13:42 +0300

    Add in LastUpdate DateTimeNow

--- Payment.Domain/Stripe/PaymentFee.cs
+++ Payment.Domain/Stripe/PaymentFee.cs
@@ -8,11 +8,11 @@ namespace Payment.Domain.Stripe
 {
     public class PaymentFee : Entity<int>
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string PaymentMethod { get; set; }
         public decimal PercentageFee { get; set; }
         public decimal FixedFee { get; set; }
         public string Currency { get; set; }
-        public DateTime LastUpdated { get; set; }
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow; 
     }
 }
